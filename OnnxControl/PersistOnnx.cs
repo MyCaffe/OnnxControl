3b4b005327f88425cbafdff887dc0e3963ba800d
ZoroDerVonCodier <daveb@signalpop.com>
2020-05-23 19:05:42 -0700

    Initial commt.

---
+++ OnnxControl/PersistOnnx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Protobuf;
+using System.Collections;
+using Onnx;
+
+/// <summary>
+/// The OnnxControl namespace.
+/// </summary>
+namespace OnnxControl
+{
+    /// <summary>
+    /// The PersistOnnx class is used to load and save models (including weights) in the .onnx format.
+    /// </summary>
+    /// <remarks>
+    /// @see [ONNX Syntax](https://github.com/onnx/onnx/blob/master/docs/IR.md) for more information on the ONNX model file format.
+    /// </remarks>
+    public class PersistOnnx
+    {
+        /// <summary>
+        /// The PersistOnnx constructor.
+        /// </summary>
+        public PersistOnnx()
+        {
+        }
+
+        /// <summary>
+        /// Returns the latest ONNX version supported.
+        /// </summary>
+        public string LatestSupportedVersion
+        {
+            get { return "1.7"; }
+        }
+
+        /// <summary>
+        /// Load an .onnx model file and return the data in an Onnx.ModelProto.
+        /// </summary>
+        /// <param name="strFile">Specifies the .onnx model file.</param>
+        /// <returns>The loaded model is returned in the Onnx.ModelProto which includes the model architecture, weights and optionally, input data.</returns>
+        public ModelProto Load(string strFile)
+        {
+            using (FileStream fs = new FileStream(strFile, FileMode.Open, FileAccess.Read))
+            {
+                return ModelProto.Parser.ParseFrom(fs);
+            }
+        }
+
+        /// <summary>
+        /// Save the data within a Onnx.ModelProto to an .onnx model file.
+        /// </summary>
+        /// <param name="model">Specifies the model data.</param>
+        /// <param name="strFile">Specifies the output file where the ModelProto data is to be saved.</param>
+        public void Save(ModelProto model, string strFile)
+        {
+            using (FileStream fs = new FileStream(strFile, FileMode.CreateNew, FileAccess.Write))
+            {
+                model.WriteTo(fs);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# OnnxControl
-The OnnxControl is an open-source technology that gives Windows C# developers easy access to the open ONNX AI model format.
+# AiModelXControl
+The OnnxControl is an open-source technology that gives Windows C# developers easy access to the open ONNX AI model format provided by ONNX at https://onnx.ai.
+
+MyCaffe uses the OnnxControl to convert between the MyCaffe (and CAFFE) model formats and the ONNX model format.
