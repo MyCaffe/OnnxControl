c90c00d3efcdad28aff8dd864d566fb3902687af
Dave Brown <daveb@signalpop.com>
2022-04-18 09:38:18 -0700

    Added test application

---
+++ OnnxControl.app.Test/FormMain.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace OnnxControl.app.Test
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadMnist = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLoadMnist
+            // 
+            this.btnLoadMnist.Location = new System.Drawing.Point(33, 33);
+            this.btnLoadMnist.Name = "btnLoadMnist";
+            this.btnLoadMnist.Size = new System.Drawing.Size(142, 23);
+            this.btnLoadMnist.TabIndex = 0;
+            this.btnLoadMnist.Text = "Load MNIST";
+            this.btnLoadMnist.UseVisualStyleBackColor = true;
+            this.btnLoadMnist.Click += new System.EventHandler(this.btnLoadMnist_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLoadMnist);
+            this.Name = "Form1";
+            this.Text = "OnnxControl Test Application";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadMnist;
+    }
+}
+
---
+++ OnnxControl.app.Test/FormMain.cs
@@ -0,0 +1,64 @@
+﻿using Onnx;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OnnxControl.app.Test
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void getModelFile(string strUrl, string strPath)
+        {
+            if (!File.Exists(strPath))
+            {
+                using (WebClient client = new WebClient())
+                {
+                    Trace.WriteLine("Downloading '" + strUrl + "' - this may take awhile...");
+                    client.DownloadFile(strUrl, strPath);
+                }
+            }
+        }
+
+        private void btnLoadMnist_Click(object sender, EventArgs e)
+        {
+            PersistOnnx persist = new PersistOnnx();
+
+            string strTestPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + "\\MyCaffe\\test_data\\models\\onnx";
+            if (!Directory.Exists(strTestPath))
+                Directory.CreateDirectory(strTestPath);
+
+            string strModelFileBig = strTestPath + "\\bvlcalexnet-9.onnx";
+            //string strDownloadPathBig = "https://github.com/onnx/models/blob/main/vision/classification/alexnet/model/bvlcalexnet-9.onnx";
+            string strModelFileSmall = strTestPath + "\\mnist-1.onnx";
+            string strDownloadPathSmall = "https://github.com/onnx/models/blob/main/vision/classification/mnist/model/mnist-1.onnx";
+            string strDownloadPath = strDownloadPathSmall;
+            string strModelFile = strModelFileSmall;
+
+            getModelFile(strDownloadPath, strModelFile);
+
+            ModelProto model = persist.Load(strModelFile);
+
+            Trace.WriteLine("Loaded model file '" + strModelFile + "'...");
+            Trace.WriteLine("Version = " + model.IrVersion.ToString());
+            Trace.WriteLine("Producer Name = " + model.ProducerName);
+            Trace.WriteLine("Producer Version = " + model.ProducerVersion);
+            Trace.WriteLine("Model Version = " + model.ModelVersion.ToString());
+            Trace.WriteLine("Description = " + model.DocString);
+            Trace.WriteLine("Domain = " + model.Domain);
+        }
+    }
+}
